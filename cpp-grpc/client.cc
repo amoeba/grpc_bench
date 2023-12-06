e7e7526f9022a09c84fbb2d9bd9c20a9d1d20512
Bryce Mecum <petridish@gmail.com>
2023-12-05 17:53:38 -0900

    Add calculation of average throughput to C++ example

--- cpp-grpc/client.cc
+++ cpp-grpc/client.cc
@@ -51,34 +51,51 @@ private:
   std::unique_ptr<grpc_bench::DataService::Stub> stub_;
 };
 
-void RunMain(DataServiceClient &client) {
+double RunMain(DataServiceClient &client) {
   auto start = std::chrono::high_resolution_clock::now();
   auto bytes_read = client.GiveMeData();
 
   if (bytes_read < 0) {
     std::cout << "Encountered an unexpected error when calling RPC. Exiting."
               << std::endl;
-    return;
+    return (double)-1;
   }
 
   auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> duration = end - start;
+  double throughput =
+      (((double)bytes_read / 1024 / 1024 / 1024) / duration.count());
+  std::cout << throughput << " GB/s" << std::endl;
 
-  std::cout << (((double)bytes_read / 1024 / 1024 / 1024) / duration.count())
-            << " GB/s" << std::endl;
+  return throughput;
+}
 
-  return;
+double mean(double *values, int n) {
+  double sum = 0;
+
+  for (int i = 0; i < n; i++) {
+    sum += values[i];
+  }
+
+  return sum / (double)n;
 }
 
 int main(int argc, char **argv) {
   absl::ParseCommandLine(argc, argv);
   std::string target_str = absl::GetFlag(FLAGS_target);
+  int ntimes = absl::GetFlag(FLAGS_ntimes);
+
   DataServiceClient client(
       grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
 
-  for (int i = 0; i < absl::GetFlag(FLAGS_ntimes); i++) {
-    RunMain(client);
+  double values[ntimes];
+
+  for (int i = 0; i < ntimes; i++) {
+    values[i] = RunMain(client);
   }
+
+  std::cout << "Average throughput: " << mean(values, ntimes) << " GB/s"
+            << std::endl;
 
   return 0;
 }
